c5f7fa214da28953ba97cadc3d2d5a9dc1556960
BigPolarBear <hanxincade@gmail.com>
2024-10-22 12:49:39 +0800

    Update dataViewer.sh

--- script/cast/dataViewer.sh
+++ script/cast/dataViewer.sh
@@ -4,22 +4,22 @@ actionId=0
 
 
 echo "joinableActions"
-cast call $dataViewerAddress "joinableActions(address,uint256)((uint256,uint256,uint256)[])" $tokenAddress $round
+cast_call $dataViewerAddress "joinableActions(address,uint256)((uint256,uint256,uint256)[])" $tokenAddress $round
 
 echo "joinedActions"
 cast_call $dataViewerAddress "joinedActions(address,address)((uint256,uint256,uint256)[])" $tokenAddress $ACCOUNT_ADDRESS
 
 echo "verifiedAddressesByAction"
-cast call $dataViewerAddress "verifiedAddressesByAction(address,uint256,uint256)((address,uint256,uint256)[])" $tokenAddress $round $actionId
+cast_call $dataViewerAddress "verifiedAddressesByAction(address,uint256,uint256)((address,uint256,uint256)[])" $tokenAddress $round $actionId
 
 echo "verificationInfosByAction"
-cast call $dataViewerAddress "verificationInfosByAction(address,uint256,uint256)(address[],string[])" $tokenAddress $round $actionId
+cast_call $dataViewerAddress "verificationInfosByAction(address,uint256,uint256)(address[],string[])" $tokenAddress $round $actionId
 
 echo "tokenDetail"
-cast call $dataViewerAddress "tokenDetail(address)(string,string,(address,uint256,uint256,uint256,uint256,uint256,bool,uint256,uint256,uint256))" $tokenAddress
+cast_call $dataViewerAddress "tokenDetail(address)(string,string,(address,uint256,uint256,uint256,uint256,uint256,bool,uint256,uint256,uint256))" $tokenAddress
 
 echo "tokenDetails"
-cast call $dataViewerAddress "tokenDetails(address[])(string[],string[],(address,uint256,uint256,uint256,uint256,uint256,bool,uint256,uint256,uint256)[])" $tokenAddress
+cast_call $dataViewerAddress "tokenDetails(address[])(string[],string[],(address,uint256,uint256,uint256,uint256,uint256,bool,uint256,uint256,uint256)[])" $tokenAddress
 
 
-cast call $dataViewerAddress "tokenDetail(address)" $tokenAddress
+cast_call $dataViewerAddress "tokenDetail(address)" $tokenAddress
