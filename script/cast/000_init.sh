cae44468ee54e15aba66c982220ffa0aaedd18cc
BigPolarBear <hanxincade@gmail.com>
2024-10-22 11:49:53 +0800

    echo all loaded vars

--- script/cast/000_init.sh
+++ script/cast/000_init.sh
@@ -11,6 +11,9 @@ source "$base_dir/address.params"
 source "$base_dir/network.params"
 source "$base_dir/LOVE20.params"
 
+# ------ user defined variables ------ 
+tokenAddress=$firstTokenAddress # 1st token
+
 
 cast_send() {
     local address=$1
@@ -59,3 +62,32 @@ check_equal(){
     fi
 }
 echo "check_equal() loaded"
+
+
+
+echo "------ user defined variables ------";
+echo "tokenAddress: $tokenAddress"
+
+echo "------ calculated variables ------";
+parentTokenAddress=$(cast_call $tokenAddress "parentTokenAddress()(address)")
+echo "parentTokenAddress: $parentTokenAddress"
+
+
+echo "------ $base_dir/.account loaded ------";
+echo "ACCOUNT_ADDRESS: $ACCOUNT_ADDRESS"
+
+echo "------ $base_dir/network.params loaded ------";
+echo "RPC_URL: $RPC_URL"
+
+echo "------ $base_dir/address.params loaded ------";
+echo "uniswapV2FactoryAddress: $uniswapV2FactoryAddress"
+echo "rootParentTokenAddress: $rootParentTokenAddress"
+echo "launchAddress: $launchAddress"
+echo "stakeAddress: $stakeAddress"
+echo "submitAddress: $submitAddress"
+echo "voteAddress: $voteAddress"
+echo "joinAddress: $joinAddress"
+echo "randomAddress: $randomAddress"
+echo "verifyAddress: $verifyAddress"
+echo "mintAddress: $mintAddress"
+echo "firstTokenAddress: $firstTokenAddress"
