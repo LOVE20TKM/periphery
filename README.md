9c8e041053b5f6cea21ffa713efa4bae0e03f355
ws1040 <29954753@qq.com>
2024-10-31 10:34:45 +0800

    uniswap v2-peripheral

--- README.md
+++ README.md
@@ -1,19 +1,13 @@
-## Foundry
-
-**Foundry is a blazing fast, portable and modular toolkit for Ethereum application development written in Rust.**
-
-Foundry consists of:
-
--   **Forge**: Ethereum testing framework (like Truffle, Hardhat and DappTools).
--   **Cast**: Swiss army knife for interacting with EVM smart contracts, sending transactions and getting chain data.
--   **Anvil**: Local Ethereum node, akin to Ganache, Hardhat Network.
--   **Chisel**: Fast, utilitarian, and verbose solidity REPL.
-
-## Documentation
-
-https://book.getfoundry.sh/
+## LOVE20 Periphery
 
 ## Usage
+
+### Install
+
+```shell
+$ cd lib/v2-periphery
+$ yarn
+```
 
 ### Build
 
--- script/DeployUniswapV2.s.sol
+++ script/DeployUniswapV2.s.sol
@@ -1,27 +1,23 @@
 // SPDX-License-Identifier: MIT
-pragma solidity =0.8.19;
+pragma solidity ^0.6.6;
 
 import "../lib/forge-std/src/Script.sol";
+import "../lib/v2-periphery/contracts/UniswapV2Router02.sol"; 
 import {Test, console} from "../lib/forge-std/src/Test.sol";
 
 contract DeployUniswapV2 is Script {
     address public uniswapV2Router02Address;
 
-    struct DeployParams {
-        address factory;
-        address WETH;
+    function _checkParams(address factory, address WETH) internal pure {
+        require(factory != address(0), "Factory address cannot be 0");
+        require(WETH != address(0), "WETH address cannot be 0");
     }
 
-    function _checkParams(DeployParams memory params) internal pure {
-        require(params.factory != address(0), "Factory address cannot be 0");
-        require(params.WETH != address(0), "WETH address cannot be 0");
-    }
-
-    function run(DeployParams memory params) external {
-        _checkParams(params);
+    function run(address factory, address WETH) external {
+        _checkParams(factory, WETH);
 
         uint256 gasLimit = 50_000_000;
-        uint256 gasPrice = 3 gwei;
+        uint256 gasPrice = 3 * 10 ** 9;
 
         vm.txGasPrice(gasPrice);
         vm.deal(msg.sender, gasLimit * gasPrice);
@@ -30,7 +26,7 @@ contract DeployUniswapV2 is Script {
         vm.broadcast();
         uniswapV2Router02Address = deployCode(
             "UniswapV2Router02.sol:UniswapV2Router02",
-            abi.encode(params.factory, params.WETH)
+            abi.encode(factory, WETH)
         );
 
         console.log("uniswapV2Router02Address: ", uniswapV2Router02Address);
